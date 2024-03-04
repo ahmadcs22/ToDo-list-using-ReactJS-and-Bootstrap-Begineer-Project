3071f848af14fd44164458b10819b72020d37b55
Ahmad Siraj <ahmadsirajsuperior@gmail.com>
2024-03-05 00:36:10 +0500

    added deafult  prop types

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,14 +1,14 @@
-// import logo from './logo.svg';
+
 import './App.css';
 import { Footer } from './MyComponents/Footer';
 import { Header } from './MyComponents/Header';
-// import { Todo } from './MyComponents/Todo';
+
 import { Todos } from './MyComponents/Todos';
 
 function App() {
   return (
     <>
-      <Header />
+      <Header title={"Todo-App"} searchBar={true} />
       <Todos />
       <Footer />
 
