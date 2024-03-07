c02f7a23beec32e7bc840a099f9b1cd4dd4aa623
Ahmad Siraj <ahmadsirajsuperior@gmail.com>
2024-03-07 23:52:03 +0500

    added about us

--- todo-list/src/MyComponents/About.js
+++ todo-list/src/MyComponents/About.js
@@ -1,8 +1,25 @@
 import React from 'react'
 
 export const About = () => {
+  let myyStyle={
+    minHeight:"80vh",
+    margin: "40px auto",
+  }
   return (
-    <div><h1>this i san about component</h1>
-    </div>
+    <>
+    <div className="container text-center my-5"><h1>About Todo List App</h1></div>
+    <div class="container " style={myyStyle}>
+    <ul>
+      <li><b><p>This Todo List App is created by Ahmad Siraj, a computer science student, as a project.</p></b></li>
+    <li><b><p>It allows you to manage your tasks efficiently by adding, deleting.</p></b></li>
+    <li><b><p>Feel free to use this app to organize your daily tasks!</p></b></li>
+    <li><b><p>If you have any feedback or suggestions for improvement, you can reach out to Ahmad Siraj via email: ahmadsirajcode@gmail.com</p></b></li>
+    <li><b><i>Ahmad Tech</i></b></li>
+    
+    
+    </ul>
+    
+</div>
+    </>
   )
 }
--- todo-list/src/MyComponents/Footer.js
+++ todo-list/src/MyComponents/Footer.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 export const Footer = () => {
   
+  
   return (
-    <footer className=" bg-dark text-light py-2">
+    <footer className=" bg-dark text-light py-2" >
       <p className='text-center'>
       Copyright &copy; mytodolist.com
       </p>
