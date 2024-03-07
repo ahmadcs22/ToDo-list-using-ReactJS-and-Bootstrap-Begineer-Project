9d41c41f85382e62e34eb4de61157cba5a44076b
Ahmad Siraj <ahmadsirajsuperior@gmail.com>
2024-03-07 23:05:36 +0500

    some chamges

--- todo-list/src/MyComponents/Todos.js
+++ todo-list/src/MyComponents/Todos.js
@@ -9,13 +9,13 @@ export const Todos = (props) => {
     <div className="container " style={myStyle}>
       <h3 className=' my-3'>Todos list</h3>
       {
-       props.todos.length===0?"No Todos To Display": props.todos.map((todo) => {
+       props.todos.length===0?<span className="badge rounded-pill text-bg-warning">No Todos To Display</span>: props.todos.map((todo) => {
         return (
           <>
           <Todoitems todo={todo} key={todo.sno} onDelete={props.onDelete}/> <hr />
           </>
         )
-      })
+      }) 
       }
 
     </div>
